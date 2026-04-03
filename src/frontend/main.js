b3a29a3aab083810f265fd60807aa1333064da92
ivunzi <ivunzi@charlotte.edu>
2026-04-02 20:33:35 -0400

    Updated the "Staff  Favorites" images on the Index Section

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const searchBtn = document.getElementById('searchBtn');
-    
     if (searchBtn) {
         searchBtn.addEventListener('click', () => {
             const val = document.getElementById('searchInput').value;
             if(val) {
                 document.getElementById('statusText').innerText = `Searching for "${val}"...`;
+            } else {
+                alert("Please enter a book title or author.");
             }
         });
     }
