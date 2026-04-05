065ce7fbdfc83fa2be1a14839c25fc072b559460
Sana Ullah <sullah20@bradford.ac.uk>
2026-04-05 17:24:23 -0400

    Add seed data for books in seed.sql

---
+++ src/database/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO books (title, author, isbn, genre, total_copies, available_copies, status)
+VALUES
+('Clean Code', 'Robert C. Martin', '9780132350884', 'Programming', 3, 3, 'available'),
+('The Pragmatic Programmer', 'Andrew Hunt', '9780201616224', 'Programming', 2, 2, 'available'),
+('Introduction to Algorithms', 'Thomas H. Cormen', '9780262046305', 'Computer Science', 1, 1, 'available');
