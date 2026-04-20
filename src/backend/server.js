a60301594cd1e150c21c8b0e8667c33c377d5f09
warment1 <warment1@charlotte.edu>
2026-04-19 20:51:44 -0400

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -39,6 +39,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json({ limit: '10kb' }));
 app.use(express.static(path.join(__dirname, '../frontend')));
 
